82bff7e0b5884cb19c4ebee0962498d60ac4a176
AparnaAppus <sp.aparna765@gmail.com>
2018-03-16 19:21:20 +0530

    deleted

--- app/static/layout/scripts/logic.js
+++ app/static/layout/scripts/logic.js
@@ -1,20 +1,9 @@
-var myHash = location.hash;
-//console.log(x);
-var link = location.protocol + "//" +location.host
-$(document).ready(function() {
-  // $.ajax({
-  //     url: (link + '/assetList/' + myHash.split("#")[1]),
-  //     type: 'get',
-  //     //  contentType: 'application/json',
-  //     // data: JSON.stringify( { "uname": username, "pwd": pws} ),
-  //     success: function(data, st, xhr) {
-  //       console.log(data);
-  //     },
-  //     error: function(returnval) {
-  //
-  //     }
-  //   });
+function highlightMe(id) {
+  var highlightedElements = document.getElementsByClassName('highlighted')
+  for (var i = 0; i < highlightedElements.length; i++) {
+    highlightedElements[i].classList.remove('highlighted')
+  }
+  // document.getElementById(id).classname += "highlighted"
+  document.getElementById(hash).style.backgroundColor="Yellow";
 
-
-
-});
+}
